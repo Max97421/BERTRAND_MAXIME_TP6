08263a4ac87baeb3c5cb99293df1bdc46f376da0
Max97421 <95446273+Max97421@users.noreply.github.com>
2022-02-13 23:46:06 +0100

    Add files via upload

---
+++ BuildAPizza/Pizza.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace BuildAPizza
+{
+    
+        enum Dough{
+            White, Brown
+        }
+        
+        enum Cheese{
+            White, Brown
+        }
+        
+        enum Topping{
+            Kittens, Onions, Salami
+        }
+        
+        class Pizza{
+
+            private Dough dough;
+            private Cheese cheese;
+            private HashSet<Topping> toppings;
+
+            public Pizza(Dough dough, Cheese cheese, HashSet<Topping> toppings){
+                this.dough = dough;
+                this.cheese = cheese;
+                this.toppings = toppings;
+            }
+
+            //add some getters
+
+        }
+        
+        class PizzaBuilder{
+
+            private Dough dough;
+            private Cheese cheese;
+            private HashSet<Topping> toppings = new HashSet<Topping>();
+
+            public PizzaBuilder(){
+
+            }
+            public PizzaBuilder setDough(Dough dough){
+                this.dough = dough;
+                return this;
+            }
+            public PizzaBuilder setCheese(Cheese cheese){
+                this.cheese = cheese;
+                return this;
+            }
+            public PizzaBuilder addTopping(Topping topping){
+                this.toppings.Add(topping);
+                return this;
+            }
+            public Pizza buildPizza(){
+                return new Pizza(this.dough, this.cheese, this.toppings);
+            }
+
+        }
+        
+    }
---
+++ BuildAPizza/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace BuildAPizza
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pizza myPizza = new PizzaBuilder()
+                .setDough(Dough.White)
+                .setCheese(Cheese.White)
+                .addTopping(Topping.Kittens)
+                .addTopping(Topping.Salami)
+                .buildPizza();
+        }
+    }
+}
